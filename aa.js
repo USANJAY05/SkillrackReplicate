c77c43f605b6ae56dee1a6be0b21f1385e880337
USANJAY05 <115535475+USANJAY05@users.noreply.github.com>
2023-10-29 11:14:02 +0530

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,7 @@
+document.getElementById("1").innerHTML="In a Zoo there are some birds and animals . All birds have two legs and all animals have four legs. Given the head count and leg count of both birds and animals taken together, the program must print the head count of birds and animals seperated by a space as output. ";
+document.getElementById("2").innerHTML="first line will contain the integer value h representing the head count of both birds and animals taken together. second line will contain integer value l representing the leg count of value l representing the leg count of both birds and animals taken together.";
+document.getElementById("3").innerHTML="first line will contain the integer values of the head count of birds and animals separated by a space.";
+document.getElementById("6").innerHTML="There are 12 birds and 15 animals.";
+document.getElementById("7").innerHTML="256";
+document.getElementById("8").innerHTML="100 14";
+document.getElementById("9").innerHTML="There are 100 birds and 14 animals.";
